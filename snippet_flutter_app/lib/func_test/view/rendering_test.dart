5fbf9a3eb34b8d308e8662974278c9b383d5681e
kkglpp <kkglpp@gmail.com>
2024-11-06 10:00:39 +0900

    첫페이지 일부 점검

---
+++ snippet_flutter_app/lib/func_test/view/rendering_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class RenderingTest extends StatelessWidget {
+  const RenderingTest({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
